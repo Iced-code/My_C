87ce8fa398a3fad229d90fb88195f62716c6825a
Ayaan Modak <ayaanmodak1@gmail.com>
2022-05-12 00:10:12 -0400

    Caller Program

--- test/main.c
+++ test/main.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 
 void call() {
-	printf("Calling User\n");
+	printf("Calling User...\n");
+}
+
+bool midCall(bool x) {
+	return x;
 }
 
 
 int main() {
 	call();
 
-	printf("Call in progress...\n");
+	if (midCall(false) == true) {
+
+		printf("Call In Progress...\n");
+
+	}
+	else {
+		printf("Call Failed. Try Again Later.");
+	}
 }
